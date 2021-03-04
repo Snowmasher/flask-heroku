fe211c58e049621fe82d71ce50024b6e8508d981
Snowmasher <alvaroroblesortega@gmail.com>
2021-03-04 22:46:40 +0100

    first

---
+++ flaskHeroku.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, jsonify
+from flask import render_template
+
+
+import sys
+import json
+
+from flask.views import MethodView
+
+
+app= Flask(__name__)
+
+@app.route("/<string:name>")
+def hello(name):
+  return render_template("home.html", name=name)
+
+
+if __name__ == '__main__':
+  app.run(port=5300,debug=True)
+
